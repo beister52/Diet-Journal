173c99e78b4b20ced9791ea54fd566961fc5af2c
beister52 <brayden.eister@gmail.com>
2025-07-14 15:45:26 -0400

    initial dotnet commit

---
+++ v2/dj_backend/Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace dj_backend.Models {
+    public class FoodEntry {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateOnly Date { get; set; }
+        public List<string> Ingredients { get; set; }
+        public bool GotSick { get; set; }
+    }
+}
