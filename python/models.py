9f67f97032ec8b99990a7bd76373e1a61674eca9
beister52 <brayden.eister@gmail.com>
2026-01-16 16:14:46 -0500

    Removed v1 Python code

--- python/models.py
+++
@@ -1,7 +0,0 @@
-# Data wrapper for food entries
-class FoodEntry():
-    def __init__(self, date, name, ingredients, sick):
-        self.date = date
-        self.name = name
-        self.ingredients = ingredients
-        self.sick = sick
